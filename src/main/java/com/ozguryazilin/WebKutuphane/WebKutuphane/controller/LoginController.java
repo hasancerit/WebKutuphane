7640ac0d5b1e78fa89b16a2e3aa7bcc605ee0ad9
hasancerit <hasan__cerit@hotmail.com>
2020-03-12 15:59:46 +0300

    login method

---
+++ src/main/java/com/ozguryazilin/WebKutuphane/WebKutuphane/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.ozguryazilin.WebKutuphane.WebKutuphane.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login/login";
+    }
+}
